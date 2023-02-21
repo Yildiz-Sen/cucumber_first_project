4dedd8f0980fd32990fa76df254e5de30f68be8f
Yildiz Sen <yildizsen93@gmail.com>
2023-02-21 15:03:08 -0600

    pushed

--- src/test/java/steps/TechGlobalPaginationSteps.java
+++ src/test/java/steps/TechGlobalPaginationSteps.java
@@ -2,6 +2,7 @@ package steps;
 
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.cucumber.datatable.DataTable;
